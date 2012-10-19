9c53e12c43edceebe7223fbbbaa1c1760c4bb5e9
BoHengOrz <BoHengOrz@gmail.com>
2012-10-20 02:12:29 +0800

    刪除R-tree Service

--- RTreeService/src/Constant.java
+++
@@ -1,7 +0,0 @@
-
-public class Constant {
-	public static final int PORT = 9999;
-	public static final char RULE_INITIAL = '0';
-	public static final char RULE_INSERT = '1';
-	public static final char RULE_QUERY = '2';
-}
